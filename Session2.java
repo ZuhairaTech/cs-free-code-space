2a91b9d1b32eb86ccaabdb44a44b94ca922138a5
ZuhairaTech <zuhairanasrin.zakaria.com>
2026-01-18 11:14:33 +0800

    9362 session2

---
+++ Session2.java
@@ -0,0 +1,13 @@
+public class Session2 {
+    public static void main(String[] args){
+
+        int num1 = 10; // INPUT num1
+        int num2 = 20; // INPUT num2
+        int num3 = num1 + num2; // num3 = num1 + num2
+        int jumlah = num3; // jumlah = num3
+        int purata = jumlah / 2; // purata = jumlah / 2
+        System.out.println("Purata: " + purata); // PAPAR purata
+
+     }
+
+}
