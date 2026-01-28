97948125f9641a0fc2c923cccab792779a0bf9d3
ShyCoffee <141325835+ZuhairaTech@users.noreply.github.com>
2026-01-28 15:25:23 +0800

    Practical for tonight lesson

---
+++ 9362/Practical2.java
@@ -0,0 +1,11 @@
+// Java Example
+public class Practical2 {
+    public static void main(String[] args) {
+        //TODO: Masukkan nombor pertama
+        //TODO: Masukkan nombor kedua
+        //TODO: Masukkan jenis operasi iaitu sama ada tambah atau darab
+        //TODO: Sekiranya operasi ialah tambah, maka jumlahkan nombor pertama dan kedua
+        //TODO: Sekiranya operasi ialah darab, maka darabkan nombor pertama dan kedua
+        //TODO: Paparkan jawapan
+    }
+}
