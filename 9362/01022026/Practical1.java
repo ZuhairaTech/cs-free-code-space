c9e4afb4ea8fc3de11aa5f7cea59f5324a74436f
ZuhairaTech <zuhairanasrin.zakaria@gmail.com>
2026-02-01 11:15:09 +0800

    deploy latest exercise code

---
+++ 9362/01022026/Practical1.java
@@ -0,0 +1,17 @@
+public class Practical1 {
+  public static void main(String[] args) {
+    // MULA
+    int isnin = 120; // INPUT isnin
+    int selasa = 80; // INPUT selasa
+    int rabu = 100; // INPUT rabu
+    int khamis = 90; // INPUT khamis
+    int jumaat = 70; // INPUT jumaat
+    int sabtu = 200; // INPUT sabtu
+    
+    int jumlah=isnin + selasa + rabu + khamis + jumaat + sabtu; // KIRA jumlah
+        
+    int purata=jumlah/6; // KIRA purata
+        
+    System.out.print(purata); // PAPAR purata
+  }
+}
