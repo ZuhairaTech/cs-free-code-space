faa5655faa5a85f16086bab367061fb0270afa39
ZuhairaTech <zuhairanasrin.zakaria@gmail.com>
2026-02-06 21:06:15 +0800

    code from session 4.2

---
+++ 9362/04022026/latihan1Soalan1.java
@@ -0,0 +1,10 @@
+public class latihan1Soalan1 {
+    public static void main(String[] args) {
+       float y=20;
+       y=y*5;
+       y=y/2;
+       y=y*0.5f+y;
+       System.out.print(y);
+    }
+}
+
